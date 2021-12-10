f30349f4349ccef25df2c0e86d36ea5273bb1e70
jackbap17 <91566093+jackbap17@users.noreply.github.com>
2021-12-09 23:23:40 -0700

    Update handle_collisions_action.py

--- game/handle_collisions_action.py
+++ game/handle_collisions_action.py
@@ -11,10 +11,23 @@ class HandleCollisionsAction(Action):
         ship = cast["ship"][0]
         missiles = cast["missiles"] 
         aliens = cast["aliens"]
-
+        
         for alien in aliens:
             for missile in missiles:
                 if missile.get_position().equals(alien.get_position()):
                     alien.set_text(" ")
-                    missile.set_text(" ")
+                    missile.set_text(' ')
+                if missile.get_position().get_y() == 1:
+                    missile.set_text(' ')
+
+        a= 0                    
+        for alien in aliens:
+            if alien.get_text() == " ":
+                cast["aliens"].pop(a)
+            a +=1
+        m = 0
+        for missile in missiles:
+            if missile.get_text == " ":
+                cast["missiles"].pop(m)
+            m +=1           
 
