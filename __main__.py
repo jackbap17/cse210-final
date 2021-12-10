c3d43228b2c055fb46eebdd4fe4c76682ce488a8
jackbap17 <91566093+jackbap17@users.noreply.github.com>
2021-12-09 23:22:53 -0700

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -10,20 +10,31 @@ from game.move_actors_action import MoveActorsAction
 from game.input_service import InputService
 from game.output_service import OutputService
 from asciimatics.screen import Screen
+from game.create_cast import CreateCast
 
 def main(screen):
 
     cast = {}
+    cast["missiles"] = []
+    cast["aliens"] = []
+    create_cast = CreateCast()
+    create_cast.create_ship(cast)
 
-    x = int(constants.MAX_X / 2)
-    y = int(constants.MAX_Y - 1)
-    position = Point(x, y)
-    ship = Actor()
-    ship.set_text("A")
-    ship.set_position(position)
 
-    cast["ship"] = [ship]
+    script = {}
 
-    cast["alien"] = []
+    input_service = InputService(screen)
+    output_service = OutputService(screen)
+    control_actors_action = ControlActorsAction(input_service)
+    move_actors_action = MoveActorsAction()
+    handle_collisions_action = HandleCollisionsAction()
+    draw_actors_action = DrawActorsAction(output_service)
 
-    
+    script["input"] = [control_actors_action]
+    script["update"] = [move_actors_action, handle_collisions_action]
+    script["output"] = [draw_actors_action]
+
+    director = Director(cast, script)
+    director.start_game()
+
+Screen.wrapper(main)    
